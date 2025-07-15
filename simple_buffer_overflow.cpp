fc54fb53f5fd3ccd110554ad15917ce3f75d94cc
formatc2013 <formatc2013@gmail.com>
2025-07-15 23:43:16 +0200

    wip

--- simple_buffer_overflow.cpp
+++ simple_buffer_overflow.cpp
@@ -5,12 +5,8 @@
 
 void sayhello(){
 	
-	std::cout << "what is your name?" << std::endl;
 	char buffer[50];
 	std::cin >> buffer;
-        
-	std::cout <<"hey " << buffer << "! I am sure you can do something wicked with this little app!" << std::endl;
-
 }
 int main(){
 	
